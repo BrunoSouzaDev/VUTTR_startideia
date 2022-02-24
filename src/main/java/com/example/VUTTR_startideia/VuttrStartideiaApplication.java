bc393aa7fe1abc4187e75421bb60b8bfac4fc4d1
Bruno Souza <bruno1994deilson@gmail.com>
2022-02-23 21:06:57 -0300

    New project - first commit

---
+++ src/main/java/com/example/VUTTR_startideia/VuttrStartideiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.VUTTR_startideia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrStartideiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrStartideiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VUTTR_startideia/VuttrStartideiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VUTTR_startideia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuttrStartideiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
