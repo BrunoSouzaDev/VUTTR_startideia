7f6f056697dd2853a0384564b69592caa2c48057
Bruno Souza <bruno1994deilson@gmail.com>
2022-02-25 10:59:15 -0300

    First prototype

--- src/main/java/com/example/VUTTR_startideia/repository/ToolRepository.java
+++ src/main/java/com/example/VUTTR_startideia/repository/ToolRepository.java
@@ -7,15 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class ToolRepository extends JpaRepository<Tool, Long> {
+public interface ToolRepository extends JpaRepository<Tool, Long> {
 
     public List<Tool> findByDsTool(String dsTool);
 
     public List<Tool> findByDsTollIgnoreCaseContaining(String dsTool);
 
     @Query(value = "select * from tool where ds_tool like '%:dsTool%'", nativeQuery = true)
-    public List<Tool> findContainingDsProduto(@Param("dsTool")String dsTool);
+    public List<Tool> findContainingDsTool(@Param("dsTool")String dsTool);
 
     @Query("select p.idTool from tool p")
     public List<Long> buscarIdsUtilizados();
